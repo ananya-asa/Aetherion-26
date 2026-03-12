a056c37bde2938671875ef8f03a0b07b28053380
Ananya ASA <ananyakayarthadka624@gmail.com>
2026-03-12 20:49:21 +0000

    feat: add Flask health prediction API

--- ai_model/app.py
+++ ai_model/app.py
@@ -1,3 +1,5 @@
+import os
+sed -i '1s/^/import os\n/' app.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
@@ -103,4 +105,5 @@ def health():
     return jsonify({'status': 'ok'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
